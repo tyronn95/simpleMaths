fec24c4f731c097b318fe1fa74e402fcad7cbe54
Boss <kamtchi95@gmail.com>
2024-03-29 12:26:55 +0100

    Function Add

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ function Calculatrice(nombre) {
     return nombre;
   }
 
-  function Subtraction(nb1, nb2) {
-    result = nb1 - nb2;
-    return result;
-  }
+function Subtraction(nb1, nb2) {
+  result = nb1 - nb2;
+  return result;
+}
+
+function Add(a, b){
+  return a + b;
+}
+
+function Mul(n1, n2) {
+  return n1 * n2;
+}
